83fff81fee9ff000b6c677ebd39e24a440c4a899
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-04-03 15:50:33 +0900

    feat: 🎸 added spanish

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import "fmt"
 const enHelloPrefix = "Hello, "
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
 
-func Hello(name string) string {
+func Hello(name string, lang string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if lang == "Spanish" {
+		return "Hola, " + name
 	}
 	return enHelloPrefix + name
 }
